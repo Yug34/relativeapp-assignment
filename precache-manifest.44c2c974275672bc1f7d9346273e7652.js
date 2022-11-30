a75204d55c339c65d2358885d7eb9f91f4dd1d8e
Yug34 <yug.gjr@gmail.com>
2022-11-30 20:10:31 +0530

    Updates

---
+++ precache-manifest.44c2c974275672bc1f7d9346273e7652.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6ddd893c15bcb542d20782fa5706e7",
+    "url": "/relativeapp-assignment/index.html"
+  },
+  {
+    "revision": "5affb2dbb07833e316b0",
+    "url": "/relativeapp-assignment/static/css/main.1dffabea.chunk.css"
+  },
+  {
+    "revision": "d89a8e2cd31cbf590620",
+    "url": "/relativeapp-assignment/static/js/2.38f61541.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/relativeapp-assignment/static/js/2.38f61541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2a0222cf58b168c2e3",
+    "url": "/relativeapp-assignment/static/js/3.c53f5719.chunk.js"
+  },
+  {
+    "revision": "5affb2dbb07833e316b0",
+    "url": "/relativeapp-assignment/static/js/main.ebac4739.chunk.js"
+  },
+  {
+    "revision": "81b99f1e05fe5ea90bf7",
+    "url": "/relativeapp-assignment/static/js/runtime-main.c34e1892.js"
+  },
+  {
+    "revision": "450d1543f34f98446e4c42acf8131c1f",
+    "url": "/relativeapp-assignment/static/media/CardContent.450d1543.svg"
+  },
+  {
+    "revision": "0ffedf11eb0899c3fb102fa1d99576ef",
+    "url": "/relativeapp-assignment/static/media/activity.0ffedf11.svg"
+  },
+  {
+    "revision": "4d554ceac66b08498ac63cda47549def",
+    "url": "/relativeapp-assignment/static/media/binance.4d554cea.svg"
+  },
+  {
+    "revision": "a15414bc138e960f9f67a0afc8215b9a",
+    "url": "/relativeapp-assignment/static/media/bitcoin.a15414bc.svg"
+  },
+  {
+    "revision": "d64eceb3ee8b5665c406dd9d8976d744",
+    "url": "/relativeapp-assignment/static/media/eth.d64eceb3.svg"
+  },
+  {
+    "revision": "863f9b336aee9fae0a5818c1b77a1400",
+    "url": "/relativeapp-assignment/static/media/shib.863f9b33.svg"
+  },
+  {
+    "revision": "ee0c9d5dcb15d88589e6b9e93f4f3dea",
+    "url": "/relativeapp-assignment/static/media/solana.ee0c9d5d.svg"
+  }
+]);
